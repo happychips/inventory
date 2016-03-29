b1bf7c2d03e0991347462a9c515e913985e5cc0e
Nikolas Martens <Nikolas.M@rtens.org>
2016-03-29 13:01:52 +0600

    UI integration

--- src/app/Launcher.php
+++ src/app/Launcher.php
@@ -2,15 +2,24 @@
 namespace happy\inventory\app;
 
 use happy\inventory\AcquireMaterial;
+use happy\inventory\AddCostumer;
 use happy\inventory\ConsumeMaterial;
+use happy\inventory\DeliverProduct;
 use happy\inventory\ListAcquisitions;
+use happy\inventory\ListCostumers;
 use happy\inventory\ListMaterials;
+use happy\inventory\ListProducts;
 use happy\inventory\model\AcquisitionIdentifier;
+use happy\inventory\model\CostumerIdentifier;
 use happy\inventory\model\MaterialIdentifier;
+use happy\inventory\model\ProductIdentifier;
+use happy\inventory\ProduceProduct;
 use happy\inventory\ReceiveDelivery;
 use happy\inventory\RegisterMaterial;
+use happy\inventory\RegisterProduct;
 use happy\inventory\ShowHistory;
 use happy\inventory\UpdateInventory;
+use happy\inventory\UpdateStock;
 use rtens\domin\delivery\web\adapters\curir\root\IndexResource;
 use rtens\domin\delivery\web\WebApplication;
 use rtens\domin\reflection\GenericMethodAction;
@@ -57,16 +66,27 @@ class Launcher {
             $domin->identifiers->setProvider(AcquisitionIdentifier::class, function () {
                 return $this->app->handle(new ListAcquisitions())->getAcquisitions();
             });
+            $domin->identifiers->setProvider(ProductIdentifier::class, function () {
+                return $this->app->handle(new ListProducts())->getProducts();
+            });
+            $domin->identifiers->setProvider(CostumerIdentifier::class, function () {
+                return $this->app->handle(new ListCostumers())->getCostumers();
+            });
         }, WebDelivery::init()));
     }
 
     private function addActions(WebApplication $domin) {
         if ($this->session->isLoggedIn()) {
             $this->addAction($domin, RegisterMaterial::class);
+            $this->addAction($domin, RegisterProduct::class);
+            $this->addAction($domin, AddCostumer::class);
             $this->addAction($domin, AcquireMaterial::class);
             $this->addAction($domin, ReceiveDelivery::class);
             $this->addAction($domin, ConsumeMaterial::class);
             $this->addAction($domin, UpdateInventory::class);
+            $this->addAction($domin, ProduceProduct::class);
+            $this->addAction($domin, UpdateStock::class);
+            $this->addAction($domin, DeliverProduct::class);
             $this->addAction($domin, ShowHistory::class)->setModifying(false);
         } else {
             $domin->actions->add('Login', (new GenericMethodAction($this, 'login', $domin->types, $domin->parser))->generic()->setCaption('Login'));
